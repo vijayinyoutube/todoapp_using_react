82b7abb0c26e838c8c64c18264cbccf31f9fb9c7
vijayinyoutube <vijaythevirtue@gmail.com>
2023-09-14 20:59:42 +0530

    refactor

--- src/Components/input_field.tsx
+++ src/Components/input_field.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, Divider, Input, List, Modal, Typography } from "antd";
+import { Alert, Checkbox, Divider, Input, List, Modal, Typography } from "antd";
 import Search from "antd/es/input/Search";
 import React, { useState } from "react";
 import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
@@ -11,6 +11,7 @@ function InputComp() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [inputTextVal, setInputText] = useState();
   const [oldVal, setOldVal] = useState<any>();
+  const [edit, setEdit] = useState(false);
 
   // - - - Function to add data to local storage - - -
   const addData = (value) => {
@@ -30,20 +31,15 @@ function InputComp() {
   };
 
   // - - -  - - - - - - DELETE DATA - - - - - - - - - - -
-  const deleteData = (value: String) => {
+  const deleteData = (value: String, index: number) => {
     localStorage.removeItem(localDataKey);
-
+    console.log("Delete Index", index);
     const temp = items;
-    const deletedList = temp.filter(function (letter) {
-      return letter !== value;
-    });
-
-    setItems(deletedList);
-    console.log(deletedList);
-    localStorage.setItem(
-      localDataKey,
-      JSON.stringify(deletedList.filter((element) => element))
-    );
+    const deletedList = temp;
+    deletedList[index] = "-";
+    setItems([...deletedList]);
+    console.log("deletedList", deletedList);
+    localStorage.setItem(localDataKey, JSON.stringify(deletedList));
   };
   // - - -  - - - - - - - - - - - - - - - - - - - - - -
 
@@ -70,20 +66,19 @@ function InputComp() {
   };
 
   const handleOk = (val) => {
-    console.log("index-", val);
     let temp = items;
-    console.log("inputTxt", inputTextVal);
     let i = temp.indexOf(oldVal);
     temp[i] = inputTextVal;
-    console.log("NEW", temp);
     //  - - Replace logic here - - -
     localStorage.setItem(localDataKey, JSON.stringify(temp));
     // - - - - - - - - - - - - - - -
     setIsModalOpen(false);
+    setEdit(true);
   };
 
   const handleCancel = () => {
     setIsModalOpen(false);
+    setEdit(false);
   };
 
   const editData = (value) => {
@@ -107,59 +102,67 @@ function InputComp() {
         dataSource={items}
         renderItem={(itemVal: string, index: number) => (
           <>
-            <List.Item
-              actions={[
-                <span>
-                  <Checkbox onChange={() => completeData(index)}></Checkbox>
-                </span>,
-                <>
-                  {completed.some((item) => item === index) ? (
-                    <></>
-                  ) : (
-                    <>
-                      <Modal
-                        title="Enter new data to replace..."
-                        open={isModalOpen}
-                        onOk={(e) => handleOk(index)}
-                        okText="Edit"
-                        onCancel={handleCancel}
-                      >
-                        <Input
-                          placeholder="your notes..."
-                          onChange={(e) => handleTxtChange(e, index)}
-                        />
-                      </Modal>
-                      <a
-                        key="list-loadmore-edit"
-                        style={{ padding: "20px" }}
-                        onClick={() => editData(itemVal)}
-                      >
-                        <EditOutlined />
-                      </a>
-                    </>
-                  )}
-                  <a
-                    key="list-loadmore-more"
-                    onClick={() => deleteData(itemVal)}
-                  >
-                    <DeleteOutlined style={{ color: "red" }} />
-                  </a>
-                  ,
-                </>,
-              ]}
-            >
-              {completed.some((item) => item === index) ? (
-                <Typography.Text mark>
-                  <span style={{ textDecoration: "line-through" }}>
-                    {itemVal}
-                  </span>
-                </Typography.Text>
-              ) : (
-                <Typography.Text>
-                  <span>{itemVal}</span>
-                </Typography.Text>
-              )}{" "}
-            </List.Item>
+            {itemVal === "-" ? (
+              <>{getData()}</>
+            ) : (
+              <List.Item
+                actions={[
+                  <span>
+                    <Checkbox onChange={() => completeData(index)}></Checkbox>
+                  </span>,
+                  <>
+                    {completed.some((item) => item === index) ? (
+                      <></>
+                    ) : (
+                      <>
+                        <Modal
+                          title="Enter new data to replace..."
+                          open={isModalOpen}
+                          onOk={(e) => handleOk(index)}
+                          okText="Edit"
+                          onCancel={handleCancel}
+                        >
+                          <Input
+                            placeholder="your notes..."
+                            onChange={(e) => handleTxtChange(e, index)}
+                          />
+                          {inputTextVal || !edit ? (
+                            <></>
+                          ) : (
+                            <Alert message="This is required" type="error" />
+                          )}
+                        </Modal>
+                        <a
+                          key="list-loadmore-edit"
+                          style={{ padding: "20px" }}
+                          onClick={() => editData(itemVal)}
+                        >
+                          <EditOutlined />
+                        </a>
+                      </>
+                    )}
+                    <a
+                      key="list-loadmore-more"
+                      onClick={() => deleteData(itemVal, index)}
+                    >
+                      <DeleteOutlined style={{ color: "red" }} />
+                    </a>
+                  </>,
+                ]}
+              >
+                {completed.some((item) => item === index) ? (
+                  <Typography.Text mark>
+                    <span style={{ textDecoration: "line-through" }}>
+                      {itemVal}
+                    </span>
+                  </Typography.Text>
+                ) : (
+                  <Typography.Text>
+                    <span>{itemVal}</span>
+                  </Typography.Text>
+                )}{" "}
+              </List.Item>
+            )}
           </>
         )}
       />
